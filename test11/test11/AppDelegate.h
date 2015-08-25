bf4d8fc2ad10514801aba34f3f3cec305a1a87ab
tarena <tarena@can123.local>
2015-08-25 16:31:04 +0800

    创建一个新的工程

---
+++ test11/test11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test11
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
